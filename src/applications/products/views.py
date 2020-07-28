5c5392c97fc277e9a19abcf942683e729f0e5c84
whtslv <vladlee.2001@mail.ru>
2020-07-28 20:20:55 +0600

    modified last commit

--- src/applications/products/views.py
+++ src/applications/products/views.py
@@ -13,7 +13,7 @@ from .models import Product
 class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = 'products/list.html'
-    paginate_by = 3
+    paginate_by = 1
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
