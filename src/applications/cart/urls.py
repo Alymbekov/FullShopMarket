76f3f8e4301cdcf0c72a77db77533cfe12957c11
Alymbekov <maxim.makarov.1997@mail.ru>
2020-08-03 20:26:18 +0600

    added cart logic to project

--- src/applications/cart/urls.py
+++ src/applications/cart/urls.py
@@ -4,7 +4,9 @@ from applications.cart.views import (
     cart_home, cart_update
 )
 
+app_name = "cart"
+
 urlpatterns = [
     path('', cart_home, name="carts"),
-    path('update/', cart_update),
+    path('update/', cart_update, name="update"),
 ]
